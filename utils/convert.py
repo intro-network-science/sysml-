3343db8cd4a290d2980853dfcd11512c3ece79b8
mahdi <erfanianmahdi00@gmail.com>
2023-03-23 14:40:47 -0500

    Fix multiple empty lines

--- utils/convert.py
+++ utils/convert.py
@@ -9,23 +9,46 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 delimiter = "|"
 
+
+def _count_generator(reader):
+    b = reader(1024 * 1024)
+    while b:
+        yield b
+        b = reader(1024 * 1024)
+
+
+def get_count(file_path):
+    count = 0
+    with open(file_path, "rb") as f:
+        c_generator = _count_generator(f.raw.read)
+        # count each \n
+        count = sum(buffer.count(b'\n') for buffer in c_generator) + 1
+    return count
+
+
+count = get_count(input_file)
+
 with open(input_file, "r") as infile:
     with open(output_file, "w") as outfile:
         writer = csv.writer(outfile)
 
         row_buffer = ""
         line = infile.readline().strip()
-        while line or row_buffer:
-            if line and line.endswith("\\"):
+        i = 1
+        while line != "" and i <= count:
+            if line.endswith("\\"):
                 row_buffer += line[:-1]
-            elif line or row_buffer:
+            else:
                 row_buffer += line
 
                 row = row_buffer.split(delimiter)
                 writer.writerow(row)
                 row_buffer = ""
 
-            line = infile.readline().strip()
+            line = ""
+            while line == "" and i <= count:
+                line = infile.readline().strip()
+                i += 1
 
         if row_buffer:
             writer.writerow(row_buffer.split(delimiter))
